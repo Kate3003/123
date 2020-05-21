7b00019849a85e5de6c97abf77653c59e6d26c6c
Kate <greenfieldnectarine@gmail.com>
2020-05-21 18:54:34 +0300

    text

--- UnitTest1.cs
+++ UnitTest1.cs
@@ -11,6 +11,17 @@ namespace TestSeleniumBasic
     public class Tests
     {
         private IWebDriver driver;
+        private MainPage mainPage;
+        private const string SearchText = "user";
+        private AllProductsPage allProductsPage;
+        private const string SearchText1 = "Pie";
+        private const string SearchText2 = "Confections";
+        private const string SearchText3 = "Pavlova, Ltd.";
+        private const string SearchText4 = "12";
+        private const string SearchText5 = "31-450 g boxes";
+        private const string SearchText6 = "28";
+        private const string SearchText7 = "0";
+        private const string SearchText8 = "11";
         
         [SetUp]
         public void Setup()
@@ -23,18 +34,16 @@ namespace TestSeleniumBasic
         [Test]
         public void Login()
         {
-            driver.FindElement(By.XPath("//input[@id=\"Name\"]")).SendKeys("user");
-            driver.FindElement(By.XPath("//input[@id=\"Password\"]")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
+            mainPage = new MainPage(driver);
+            mainPage.SearchForHost(SearchText);
             Assert.AreEqual("Home page", driver.FindElement(By.XPath("//h2")).Text);
         }
 
         [Test]
         public void Logout()
         {
-            driver.FindElement(By.XPath("//input[@id=\"Name\"]")).SendKeys("user");
-            driver.FindElement(By.XPath("//input[@id=\"Password\"]")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
+            mainPage = new MainPage(driver);
+            mainPage.SearchForHost(SearchText);
             driver.FindElement(By.XPath("//a[contains(text(),'Logout')]")).Click();
             Assert.AreEqual("Login", driver.FindElement(By.XPath("//h2")).Text);
         }
@@ -42,23 +51,18 @@ namespace TestSeleniumBasic
         [Test]
         public void AddProduct()
         {
-            driver.FindElement(By.XPath("//input[@id=\"Name\"]")).SendKeys("user");
-            driver.FindElement(By.XPath("//input[@id=\"Password\"]")).SendKeys("user");
-            driver.FindElement(By.CssSelector(".btn")).Click();
-            driver.FindElement(By.XPath("(//a[contains(text(),'All Products')])[2]")).Click();
-            driver.FindElement(By.LinkText("Create new")).Click();
-            driver.FindElement(By.XPath("//input[@id=\"ProductName\"]")).SendKeys("Pie");
-            driver.FindElement(By.XPath("//select[@id=\"CategoryId\"]")).SendKeys("Confections");
-            driver.FindElement(By.XPath("//select[@id=\"SupplierId\"]")).SendKeys("Pavlova, Ltd.");
-            driver.FindElement(By.XPath("//input[@id=\"UnitPrice\"]")).SendKeys("12");
-            driver.FindElement(By.XPath("//input[@id=\"QuantityPerUnit\"]")).SendKeys("31-450 g boxes");
-            driver.FindElement(By.XPath("//input[@id=\"UnitsInStock\"]")).SendKeys("28");
-            driver.FindElement(By.XPath("//input[@id=\"UnitsOnOrder\"]")).SendKeys("0");
-            driver.FindElement(By.XPath("//input[@id=\"ReorderLevel\"]")).SendKeys("11");
-            driver.FindElement(By.CssSelector(".btn")).Click();
-            var d = "All product";
-            Assert.IsFalse(d == "Product");
-        
+            mainPage = new MainPage(driver);
+            mainPage.SearchForHost(SearchText);
+            allProductsPage = new AllProductsPage(driver);
+            allProductsPage.SearchForHost1(SearchText1);
+            allProductsPage.SearchCatId(SearchText2);
+            allProductsPage.SearchSupId(SearchText3);
+            allProductsPage.SearchUP(SearchText4);
+            allProductsPage.SearchQPU(SearchText5);
+            allProductsPage.SearchUIS(SearchText6);
+            allProductsPage.SearchUOO(SearchText7);
+            allProductsPage.SearchRL(SearchText8);
+            Assert.IsTrue(driver.FindElement(By.XPath("//*[text()=\"Pie\"]")).Displayed);
         }
 
         [TearDown]
